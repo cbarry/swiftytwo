db2cc59f5ca600ecea97af3c4295952e770016eb
cbarry <colinsbarry@gmail.com>
2015-05-23 17:37:16 -0400

    Initial Commit

---
+++ swiftytwo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  swiftytwo
+//
+//  Created by cbarry on 5/23/15.
+//  Copyright (c) 2015 cbarry. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
